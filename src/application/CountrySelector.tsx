a77ea9758f6b74ae0778b492dd4ee3e0916f651f
Noushad <noushad@eximpe.com>
2022-04-16 10:09:50 +0200

    Setup basic infrastructure

---
+++ src/application/CountrySelector.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+type Props = any;
+
+const CountrySelectorComp: React.FC<Props> = () => {
+  return <div>Something will be added here soon</div>;
+};
+
+export const CountrySelector = React.memo(CountrySelectorComp);
+export default CountrySelector;
